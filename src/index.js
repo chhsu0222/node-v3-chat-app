bd6d204d6aafa2269978b6f5fd1bf26838f095c7
ch <ch@mail.startek-eng.com>
2019-04-15 15:35:50 +0800

    Get started with socket.io
    sec. 17, video 154
    
    $ npm i socket.io@2.2.0
    
    Create the server outside of the Express library and configure
    it to use our Express app.
    Initialize a new instance of socket.io by passing the HTTP server
    object.
    When Express creates the server behind the scenes, we don't
    have access to it to pass it into socketio().

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 const app = express()
+// create the server outside of the Express library
+// and configure it to use our Express app.
+const server = http.createServer(app)
+// initialize a new instance of socket.io by passing the HTTP server object.
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port, () => {
+// listen on the connection event for incoming sockets
+io.on('connection', () => {
+    console.log('New Websocket connection')
+})
+
+server.listen(port, () => {
     console.log(`Server is up on port ${port}!`)
 })
