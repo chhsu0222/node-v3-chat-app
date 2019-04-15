7d61e498b4966b72d804d0c80e056f4c86e632c6
ch <ch@mail.startek-eng.com>
2019-04-15 17:04:12 +0800

    Socket.io events
    https://socket.io/get-started/chat/#Emitting-events
    https://socket.io/get-started/chat/#Broadcasting
    
    Using 'emit' to send events and 'on' to receive events.
    Sending data after the event's name.

--- src/index.js
+++ src/index.js
@@ -15,9 +15,27 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
+let count = 0
 // listen on the connection event for incoming sockets
-io.on('connection', () => {
+io.on('connection', (socket) => { 
+    // socket contains information about that new connection
+    // We can use methods on socket to communicate with that
+    // specific client.
+    // If we have 5 clients connecting to the server, this callback
+    // function is going to run 5 different times, one time for each
+    // connection.
     console.log('New Websocket connection')
+
+    socket.emit('countUpdated', count)
+
+    socket.on('increment', () => {
+        count++
+        // emit event to that specific connection
+        // socket.emit('countUpdated', count)
+
+        // Broadcasting - send an event to everyone
+        io.emit('countUpdated', count)
+    })
 })
 
 server.listen(port, () => {
