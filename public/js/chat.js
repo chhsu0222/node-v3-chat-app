a2de634597ed6ad6ddc16f0def4550000bd8d84a
ch <ch@mail.startek-eng.com>
2019-04-17 10:04:58 +0800

    Rendering location messages
    sec. 17, video 162
    
    Separate location from message with different events.

--- public/js/chat.js
+++ public/js/chat.js
@@ -11,11 +11,21 @@ const $messages = document.querySelector('#messages')
 
 // Templates
 const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
 
 socket.on('message', (message) => {
     console.log(message)
+    // compile the html with the data
     const html = Mustache.render(messageTemplate, {
         message
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+})
+
+socket.on('locationMessage', (url) => {
+    console.log(url)
+    const html = Mustache.render(locationMessageTemplate, {
+        url
     })
     $messages.insertAdjacentHTML('beforeend', html)
 })
