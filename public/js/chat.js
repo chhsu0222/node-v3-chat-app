7064995898c9bbf7744e57c78eee5cc8254474da
ch <ch@mail.startek-eng.com>
2019-04-18 15:53:28 +0800

    Automatic scrolling
    sec. 17, video 173
    
    https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/offsetHeight
    https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollTop
    https://developer.mozilla.org/zh-TW/docs/Web/API/Element/scrollHeight

--- public/js/chat.js
+++ public/js/chat.js
@@ -18,6 +18,31 @@ const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
 // Options
 const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
 
+const autoscroll = () => {
+    // New message element
+    const $newMessage = $messages.lastElementChild
+
+    // Height of the new message
+    const newMessageStyles = getComputedStyle($newMessage)
+    const newMessageMargin = parseInt(newMessageStyles.marginBottom)
+    const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
+
+    // Visable height
+    const visableHeight = $messages.offsetHeight
+
+    // Height of messages container
+    const containerHeight = $messages.scrollHeight
+
+    // How far have I scrolled?
+    const scrollOffset = $messages.scrollTop + visableHeight
+
+    // Make sure we were indeed at the bottom before the last message was added
+    if (containerHeight - newMessageHeight <= scrollOffset) {
+        // Scroll to the bottom
+        $messages.scrollTop = $messages.scrollHeight
+    }
+}
+
 socket.on('message', (message) => {
     console.log(message)
     // compile the html with the data
@@ -27,6 +52,7 @@ socket.on('message', (message) => {
         createdAt: moment(message.createdAt).format('h:mm a')
     })
     $messages.insertAdjacentHTML('beforeend', html)
+    autoscroll()
 })
 
 socket.on('locationMessage', (message) => {
@@ -37,6 +63,7 @@ socket.on('locationMessage', (message) => {
         createdAt: moment(message.createdAt).format('h:mm a')
     })
     $messages.insertAdjacentHTML('beforeend', html)
+    autoscroll()
 })
 
 socket.on('roomData', ({ room, users }) => {
